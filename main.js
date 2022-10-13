b82460e5c2def158f870304c08058986a9df9d3e
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-10-14 02:40:41 +0400

    13.10.2022 solved

--- main.js
+++ main.js
@@ -1,38 +1,29 @@
-// 12.10.2022 Task
-// Best time to buy and Sell stock
-// You are given array prices where prices[i] is the price of a given stock on the i-th day.
-// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-// RETURN THE MAXIMUM PROFIT YOU CAN ACHIEVE FROM THIS TRANSACTION. If you cannot achieve any profit, return 0.
+// Duplicate Zeros
+// Given a fixed-length integer array, duplicate each occurrence of zero, shifting the remaining elements to the right.
+// NOTE that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
 
-// Example 1: Input: prices = [7, 1, 5, 3, 6, 4]
-// Output: 5
+// Example 1:
+// Input:
+// Output: [1,0,0,2,3,0,0,4,5];
 
-prices = [7, 1, 5, 3, 6, 4];
 
-function maxProfit(prices) {
-    // Here we set maxProfit to zero which returns zero when no profit is made.
-    let profit = 0;
+// Time O(n^2)
+// Space O(1)
+arr = [1, 0, 2, 3, 0, 4, 5, 0];
+arr2 = [1, 2, 3];
 
-    // We compare the price of the next element in the array with the current. i is our index and j is our pointer that keeps iterating.
-    for (let i = 0; i < prices.length - 1; i++) {
-        // We use for loops to find the maximum profit. 
-        for (let j = i + 1; j < prices.length; j++) {
-            const currentProfit = prices[j] - prices[i];
-
-            if (currentProfit > profit) {
-                profit = currentProfit;
-            }
-        }
+const N = arr.length;
+for (let i = 0; i < N - 1; i++) {
+  if (arr[i] === 0) {
+    let prev = arr[i + 1];
+    for (let j = i + 2; j < N; j++) {
+      let temp = arr[j];
+      arr[j] = prev;
+      prev = temp;
     }
-
-    return profit;
+    arr[++i] = 0;
+  }
 }
 
-// This solution will have complexity of O(n^2) because we use for loop twice.
-
-// Example2: prices = [7, 6, 4, 3, 2, 1]
-
-prices2 = [7, 6, 4, 3, 2, 1];
-
-console.log(maxProfit(prices)); // Output: 5;
-console.log(maxProfit(prices2)); // Output: 0;
+console.log(arr); // Output: [1,0,0,2,3,0,0,4]
+console.log(arr2); // Output: [1,2,3]
