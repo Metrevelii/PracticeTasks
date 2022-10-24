116f3d74f447e5fc9e22f09ba84e72a72826cf3b
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-10-24 23:13:15 +0400

    18.20.2022 half

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
-// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
-// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
-// Example 1: x = 123
-// Output: 321
-// Big O - O(n^2)
+// Given a string s, return true if it is a palindrome, or false otherwise.
 
-let reverse = function(x) {
-  // Convert value to string
-  const str = "" + Math.abs(x);
-  // Get array of digit characters, reverse it and join into a string again by using chaining method
-  const reversed = str.split("").reverse().join("");
-  const num = parseInt(reversed); // Convert back to integer
-  const revNum = Math.sign(x) * num;
-  // If number is out of range - return 0
-  if (revNum < Math.pow(-2, 31) || revNum > Math.pow(2, 31) -1) return;
-  return revNum;
+// Example 1: s = "A man, a plan, a canal: Panama";
+// Output: true
+
+// const s = "A man, a plan, a canal: Panama";
+// const toCleanerStr = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+
+// const isPalindrome = (string) => {
+//   const cleanStr = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
+//   string === cleanStr.split('').reverse().join();
+//   console.log(string, cleanStr);
+// }
+
+// console.log(isPalindrome(s));
+// console.log(s.replace(/[^a-z0-9]/gi, '').toLowerCase());
+
+// console.log(isPalindrome(s));
+
+
+// console.log(isPalindrome('fof'));
+
+
+// Valid Palindrome II
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+// Example 1: Input: s = 'aba'
+// Output: true
+
+// Youtube videos shemdeg amoxsna:
+
+const isPalindrome = function(s) {
+  // Creating left pointer and assigning it to 0
+  let leftPointer = 0;
+  // Creating right pointer - assigning it to s.lenght - 1
+  let rightPointer = s.length - 1;
+  while (leftPointer < rightPointer) {
+    // If the values of pointer are not equal: find palindrome with leftPointer + 1 and rightPointer - 1 - return if any of them is true
+    if(s[leftPointer] !== s[rightPointer]) {
+      const checkLeft = checkInnerPalindrome(leftPointer + 1, rightPointer, s)
+      const checkRight = checkInnerPalindrome(leftPointer, rightPointer - 1, s)
+      return checkLeft || checkRight
+    }
+    leftPointer++
+    rightPointer--
+  }
 }
 
-console.log(reverse(123)); // Output: 321
-console.log(reverse(-123)); // Output: -321
-console.log(reverse(120)); // Output: 21
+const checkInnerPalindrome = function(leftPointer, rightPointer, s) {
+  while (leftPointer < rightPointer) {
+    if(s[leftPointer] !== s[rightPointer]) {
+      return false
+    }
+    leftPointer++;
+    rightPointer--;
+  }
+  return true;
+}
 
- 
+// Time complexity O(n)
+
