1ba43dbcb40386f337960812f316f95ff1538c6b
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-02 21:25:18 +0400

    31 10 tasks

--- main.js
+++ main.js
@@ -1,72 +1,81 @@
-// Length of Last word
-
-//Given a string s consisting of words and spaces, return the length of the last word in the string.
-// A word is a maximal substring consisting of non-space characters only.
-// Example 1: 
-const s = "Hello World";
-
-// Time O(n^2)
-
-const lastWordLength = function(s) {
-  // using trim method to remove spaces in the beggining or end, then - splitting words
-  let dividedWords = s.trim().split(' ');
-
-  // removing last element from these words, if there is no element we return 0;
-  const lastWord = dividedWords.pop() || '';
-
-  return lastWord.length;
-}
+// Given an array of integers nums which is sorted in ascending order,
+//and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
 
 
-// Example 1: 
-// console.log(lastWordLength(s));
+//example 1:
+// Input: nums = [-1, 0, 3, 5, 9, 12], target = 9;
+// Output: 9
 
-// Output: 5
+// nums = [-1, 0, 3, 5, 9, 12];
+// target = 9;
 
 
-// Example 2:
-// s = "   fly me   to   the moon  "
-// Output: 4;
+//example2: nums = [-1, 0, 3, 5, 9, 12], target = 2;
+// output: -1
+// 2 doesnt exist so it returns -1
 
-// Example 3: 
-// s = "luffy is still joyboy"
 
-// Output: 6;
+// const search = function(nums, target) {
+//   let left = 0;
+//   let right = nums.length -1;
+//   while (left <= right) {
+//     let middle = Math.floor((left + right) / 2)
+//     let potentialMatch = nums[middle];
+//     if (potentialMatch === target) {
+//       return middle;
+//     } else if (potentialMatch > target) {
+//       right = middle - 1;
+//     } else {
+//       left = middle + 1;
+//     }
+//   }
+//   return -1;
+// }
 
-console.log(lastWordLength(s));
+// 278. First Bad Version
 
-// 392. Is Subsequence
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, 
+// the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
-// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
-// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without
-// disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+// You are given an API bool isBadVersion(version) which returns whether version is bad.
+//  Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
-// Time: O(n)
+// const solution = function(isBadVersion) {
+//   return function(n) {
+//     // Min left start is 1
+//     let left = 1, right = n;
+//     while (left < right) {
+//       const mid = left + Math.floor((right - left)/ 2)
+//       if (isBadVersion(mid)) {
+//         right = mid;
+//       } else {
+//         left = mid + 1;
+//       }
+//     }
+//     return left;
+//   }
+// }
 
-// Example 1:
-// const s = "abc";
-// const t = "ahbgdc";
-// Output: true
+// console.log(isBadVersion(4))
 
-const isSubsequence = function(s, t) {
-  let i = 0
-  let j = 0;
-  // while they are within the ranges
-  while (i < s.length && j < t.length) {
-    // we compare then: if left side has same characters as right, we move them both forward:
-    if (s[i] === t[j]) {
-      i += 1
-      j += 1
 
-      // if not, we move right forward till it finds same characters
-    } else {
-      j += 1
-    }
+// 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, r
+// eturn the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+var searchInsert = function(nums, target) {
+  let left = 0, right = nums.length - 1;
+  if (target < nums[left]) return 0;
+  if (target > nums[right]) return nums.length;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (target===nums[mid]) return mid;
+    else if (target < nums[mid]) right = mid - 1;
+    else left = mid + 1;
   }
-
-  return i === s.length
-}
-
-console.log(isSubsequence("abc", "ahbgdc")); // true
-console.log(isSubsequence("axc", "ahbgdc")); // false
+};
